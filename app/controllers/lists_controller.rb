c8b2103df621bf1b012c46009e9c4e8ea2cbeb7c
Maxence Audibert <audibert.maxence@gmail.com>
2023-05-25 16:36:03 +0200

    m

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,31 @@
 class ListsController < ApplicationController
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @lists = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(params_list)
+    @list.save
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+private
+
+def params_list
+  params.require(:list).permit(:name)
+end
+
 end
